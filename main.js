115fcd85dc2333adeebb535cd354cc90b74f640a
aleks1303 <aleksbulda13@gmail.com>
2024-12-30 15:17:45 +0200

    modules http and path

--- main.js
+++ main.js
@@ -3,6 +3,10 @@
 // запустити на server
 const http = require('node:http');
 
+// дає можливість працювати з шляхами
+// будувати шляхи, нормалізовувати шляхи
+// парсити шляхи
+const path = require('path');
 
 const foo = async () => {
 // // http
@@ -37,7 +41,25 @@ const foo = async () => {
 //     server.listen(8000);
 //
 
+    // додає слеш
+    const pathToHelper = path.join(__dirname, 'helpers', 'helpers.js');
 
+    // віддає ім'я файлу з розширенням
+    console.log(path.basename(pathToHelper));
 
+    // віддає шлях до папки
+    console.log(path.dirname(pathToHelper));
+
+    // повертає розширення файлу
+    console.log(path.extname(pathToHelper));
+
+    // розбиває все на об'єкти
+    console.log(path.parse(pathToHelper));
+
+    //перебирає правильність шляху і прибирає різні зайві слеші
+    console.log(path.normalize('/Users/admin/IdeaProjects////may-2024_lessons_node-js/helpers'));
+
+    // чи цей шлях абсолютний, чи починається зі слеш
+    console.log(path.isAbsolute('/Users/admin/IdeaProjects////may-2024_lessons_node-js/helpers'));
 }
 void foo()
