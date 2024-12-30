a35ae0bbf507bd964344f2c6f9525174babcf8fa
aleks1303 <aleksbulda13@gmail.com>
2024-12-30 14:39:12 +0200

    lesson2_internal_modules

--- main.js
+++ main.js
@@ -3,37 +3,41 @@
 // запустити на server
 const http = require('node:http');
 
+
 const foo = async () => {
+// // http
+// // Create a local server to receive data from
+//     const server = http.createServer((req, res) => {
+//         res.writeHead(200, { 'Content-Type': 'application/json' });
+//         const url = req.url;
+//         if (url === '/about'){
+//             switch (req.method) {
+//                 case'GET':
+//                 return res.end(JSON.stringify({
+//                     data:'About Page'
+//                 }));
+//                 case 'POST':
+//                     return res.end(JSON.stringify({
+//                         data:'About Page'
+//                     }))
+//             }
+//
+//         }
+//         if (url === '/contact'){
+//             return res.end(JSON.stringify({
+//                 data:'Contact Page'
+//             }))
+//         }
+//
+//         res.end(JSON.stringify({
+//             data: 'Hello World!',
+//         }));
+//     });
+//
+//     server.listen(8000);
+//
 
-// Create a local server to receive data from
-    const server = http.createServer((req, res) => {
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        const url = req.url;
-        if (url === '/about'){
-            switch (req.method) {
-                case'GET':
-                return res.end(JSON.stringify({
-                    data:'About Page'
-                }));
-                case 'POST':
-                    return res.end(JSON.stringify({
-                        data:'About Page'
-                    }))
-            }
 
-        }
-        if (url === '/contact'){
-            return res.end(JSON.stringify({
-                data:'Contact Page'
-            }))
-        }
-
-        res.end(JSON.stringify({
-            data: 'Hello World!',
-        }));
-    });
-
-    server.listen(8000);
 
 }
 void foo()
